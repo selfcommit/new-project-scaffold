7d68082df93948611d53f92f03dbcfe959f2b267
Dan OBoyle <oboyledk@gmail.com>
2023-04-13 13:14:29 -0400

    add go example

--- copy_makefile.sh
+++ copy_makefile.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-APP_FOLDERS=$(find . -mindepth 1 -maxdepth 1 -type d -not -name ".*")
+# https://unix.stackexchange.com/questions/702023/
+readarray -d '' -t APP_FOLDERS < <(find . -mindepth 1 -maxdepth 1 -type d -not -name ".*" -print0 || true)
+
 COPY_FILES=(".shellcheckrc" "Makefile" ".pre-commit-config.yaml")
 
 for cf in "${COPY_FILES[@]}"; do
-	for af in "${APP_FOLDERS[@]}"; do
-		cp "${cf}" "${af}"
+	for af in "${!APP_FOLDERS[@]}"; do
+		echo "copying ""${cf}"" ""${af}""=> ${APP_FOLDERS[af]}";
+		cp "${cf}" "${APP_FOLDERS[af]}";
 	done
 done
